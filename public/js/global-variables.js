52ebac2e270c7b10b8438404d1180d37b02baada
igor <igor@igors-MacBook-Pro.local>
2017-10-11 17:08:26 +0200

    Mob menu 90% completed-need JS

--- public/js/global-variables.js
+++ public/js/global-variables.js
@@ -1,6 +1,13 @@
 var page = $('html, body');
 var windowWidth = $(window).width();
 
+var myBusiness = $(".o-nav__list--desktop__link--1st");
+var myBusiness_ddMenu = $(".o-nav__list--desktop__dd-menu--1st");
+var myVan = $(".o-nav__list--desktop__link--2nd");
+var myVan_ddMenu = $(".o-nav__list--desktop__dd-menu--2nd");
+var ddMenu = $(".o-nav__list--desktop__dd-menu");
+
+
 $(document).ready(function() {
 	$(window).resize(function() {
 		if ($(window).width() != windowWidth) {
--- source/js/menu-drop-down.js
+++ source/js/menu-drop-down.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-	$(".o-nav__list--desktop__link--1st").click(function(){
-    	$(".o-nav__list--desktop__dd-menu--1st").slideToggle(500);
+	myBusiness.click(function(){
+    	myBusiness_ddMenu.slideToggle(500);
     });
 
-    $(".o-nav__list--desktop__link--2nd").click(function(){
-    	$(".o-nav__list--desktop__dd-menu--2nd").slideToggle(500);
+    myVan.click(function(){
+    	myVan_ddMenu.slideToggle(500);
     });
 
-    $(".o-nav__list--desktop__dd-menu").click(function(){
+    ddMenu.click(function(){
 		$(".o-nav__list--desktop__dd-menu:visible").slideUp(500);
 	});
 
